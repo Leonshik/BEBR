0f8fed5ebc37ef6420304d530830e41fc348b3cc
Leonshik <98087688+Leonshik@users.noreply.github.com>
2022-03-14 16:26:18 +0700

    Add files via upload

---
+++ formula i morskoy boy/formula i morskoy boy/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+int[] kakashka1 = new int[9];
+int[,] kakashka = new int[3,3];
+int r = 0;
+int h = 0;
+int v = 0;
+for (; r <= 2; r++)
+{
+    h = 0;
+    for (; h <= 2; h++)
+    {
+        kakashka[r, h] = 3;
+        Console.Write(kakashka[r,h]);
+        Console.Write(" ");
+    }
+}
+r = 0;
+for (;r <=2;r++)
+{
+    h = 0;
+    for (;h <=2; h++)
+    {
+        kakashka1[v] = kakashka[r, h];
+        v++;        
+    }
+}
+for (int vcedg = 0; vcedg < kakashka1.Length; vcedg++)
+{
+
+    Console.Write(kakashka1 [vcedg]);
+    Console.Write(" ");
+}
+
